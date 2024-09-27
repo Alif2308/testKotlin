acc0d3e94ba50a0d6da8a1b18fc3aa00ac478dc4
Muhammad Alif Wicaksono <muhammad.aw@bankmandiri.co.id>
2024-09-27 15:46:35 +0700

    second commit

--- testKotlin/build.gradle.kts
+++ testKotlin/build.gradle.kts
@@ -54,11 +54,11 @@ publishing {
             /** Configure path of your package repository on Github
              *  Replace GITHUB_USERID with your/organisation Github userID and REPOSITORY with the repository name on GitHub
              */
-            url = uri("https://maven.pkg.github.com/GITHUB_USERID/REPOSITORY") // Github Package
+            url = uri("https://maven.pkg.github.com/Alif2308/testKotlin") // Github Package
             credentials {
                 //Fetch these details from the properties file or from Environment variables
-                username = githubProperties.get("gpr.usr") as String? ?: System.getenv("GPR_USER")
-                password = githubProperties.get("gpr.key") as String? ?: System.getenv("GPR_API_KEY")
+                username = "Alif2308"
+                password = "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"
             }
         }
     }
